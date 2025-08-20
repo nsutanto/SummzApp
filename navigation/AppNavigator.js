4bc3913270e1c4de43933e140851817cfdb98d52
nsutanto <nicholas.sutanto@gmail.com>
2025-08-19 23:46:28 -0500

    Implement Login and Main screens

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import MainScreen from '../screens/MainScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ navigation/types.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+  LoginScreen: undefined;
+  MainScreen: undefined;
+};
